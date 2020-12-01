73fc44b6caf052436d03e89f0241636dd0e41c58
Donna Engelberger <dengelberger@gmail.com>
2020-12-01 18:55:06 -0500

    Updates

--- precache-manifest.4e6dac5c05edf39374424437bfb0c9bf.js
+++ precache-manifest.4e6dac5c05edf39374424437bfb0c9bf.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1c22ee799fb92befeb18bd6def58ef6a",
+    "revision": "03e30277c7c4bfe4b35cf59a15cd7de0",
     "url": "/ReactPortfolio/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ReactPortfolio/static/css/2.c72968ad.chunk.css"
   },
   {
-    "revision": "d656eafc1d2e8c007d12",
-    "url": "/ReactPortfolio/static/css/main.9067bb3c.chunk.css"
+    "revision": "1c1ccefd13f472d464a3",
+    "url": "/ReactPortfolio/static/css/main.e79651ae.chunk.css"
   },
   {
     "revision": "3c10ff8ba5dfc1eeb83b",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ReactPortfolio/static/js/2.a33954b3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d656eafc1d2e8c007d12",
-    "url": "/ReactPortfolio/static/js/main.6ed934e6.chunk.js"
+    "revision": "1c1ccefd13f472d464a3",
+    "url": "/ReactPortfolio/static/js/main.29d0a798.chunk.js"
   },
   {
     "revision": "ab127edbe198b4268097",
